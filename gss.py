56dd1898b25bbffd4d55e6015d54db2cf26c7c2a
Adam Stanislawski <adam@asweb.pl>
2020-04-21 11:12:39 +0200

    added comments

--- gss.py
+++ gss.py
@@ -3,19 +3,28 @@ import math
 
 class GoldenSectionSearch:
     def __init__(self):
-        self.f = None
+        self.precision = None
         self.a = None
         self.b = None
 
-    def set_function(self, function):
-        """Sets reference to function that is to be minimized"""
-        self.f = function
-
-    def set_parameters(self, a, b):
-        """Sets parameters for range values"""
+    def set_parameters(self, a, b, precision):
+        """Sets parameters for range values and desired precision"""
         self.a = a
         self.b = b
+        self.precision = precision
 
     def get_extremum(self, f):
         """Gets extremum of given function f"""
         return f(0)
+
+
+'''
+--- EXAMPLE USAGE ---
+
+gss = GoldenSectionSearch()             # create directional minimizer object
+gss.set_parameters(-100, 100, 0.001)    # set parameters of directional minimizer
+
+f = lambda x: 3*(x**2) + 2*(x)          # test lambda function that is to be minimized
+extremum = gss.get_extemum(f)           # compute extremum
+
+'''
--- msd.py
+++ msd.py
@@ -1,18 +1,20 @@
 import math
+from gss import GoldenSectionSearch
+from parser import Parser
 
 
 class SteepestDescentMethod:
     def __init__(self, function, dir_minimizer):
-        self.f = function
-        self.dir_minimizer = dir_minimizer
+        self.f = function                   # calculated function
+        self.dir_minimizer = dir_minimizer  # object used for directional minimizing
 
-        self.start_value = None
-        self.precision = None
+        self.start_value = None             # vector of starting value
+        self.precision = None               # desired precision
 
-        self.steps = []
+        self.steps = []                     # array storing values of consecutive steps
 
     def set_function(self, function):
-        """Sets reference to function allowing to compute f(x1, x2)"""
+        """Sets reference to function allowing to compute f([x1, x2])"""
         self.f = function
 
     def set_directional_minimizer(self, dir_minimizer):
@@ -21,8 +23,28 @@ class SteepestDescentMethod:
 
     def set_parameters(self, start_value, precision):
         """Sets method parameters for starting position of first iteration and searched precision"""
-        self.start_value = start_value
+        self.start_value = start_value.copy()
+        self.steps.append(start_value.copy())
         self.precision = precision
 
+    def get_steps(self):
+        """Gets list of arguments values of consecutive algorithm iterations"""
+        return self.steps.copy()
+
     def compute(self):
+        """Main computing method"""
         pass
+
+'''
+--- EXAMPLE USAGE ---
+
+sdm = SteepestDescentMethod()           # create main object
+gss = GoldenSectionSearch()             # create directional minimizer object
+gss.set_parameters(-100, 100, 0.001)    # set parameters of directional minimizer
+
+sdm.set_function(lambda x: 2 * (x[0] ** 2) + 3 * (x[1] ** 2))       # pass function that is to be minimized
+sdm.set_directional_minimizer(gss)      # pass directional minimizer
+sdm.set_parameters([1, 1], 0.001)       # set parameters of the method
+
+sdm.compute()
+'''
