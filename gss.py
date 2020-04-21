fed20dcc68d64c2ff937cc527e7a94ddc9280042
Adam Stanislawski <adam@asweb.pl>
2020-04-21 18:21:19 +0200

    error fix

--- gss.py
+++ gss.py
@@ -15,16 +15,15 @@ class GoldenSectionSearch:
 
     def get_extremum(self, f):
         """Gets extremum of given function f"""
-        return f(0)
+        return f(3)
 
 
-'''
---- EXAMPLE USAGE ---
 
-gss = GoldenSectionSearch()             # create directional minimizer object
-gss.set_parameters(-100, 100, 0.001)    # set parameters of directional minimizer
+# --- EXAMPLE USAGE ---
 
-f = lambda x: 3*(x**2) + 2*(x)          # test lambda function that is to be minimized
-extremum = gss.get_extemum(f)           # compute extremum
-
-'''
+# gss = GoldenSectionSearch()             # create directional minimizer object
+# gss.set_parameters(-100, 100, 0.001)    # set parameters of directional minimizer
+#
+# f = lambda x: 3*(x**2) + 2*(x)          # test lambda function that is to be minimized
+# extremum = gss.get_extremum(f)           # compute extremum
+# print(extremum)
