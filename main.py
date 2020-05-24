1fe87f05187d4da371f77cf1ba6d09dd1ba8214a
Adam Stanislawski <adam@asweb.pl>
2020-05-24 21:50:25 +0200

    examples

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from pyqt_gui import FunctionOptimizer, QApplication
 # example function
 func = "sin(x^2 + 3y^2) / (0.1 + sqrt(x^2 + y^2)^2) + (x^2 + 5y^2) * (exp(1 - sqrt(x^2 + y^2)^2) / 2)"
 # func = "sin(0.5x^2-0.25y^2+3)*cos(2x+1-e^y)"
+
+
+# func = "5x^2 + 4y^2 + 3xy + 7x + 20" -5, 5 - ok
+# func = "10x^2 + 5xy + 8y^2 - 14x - 6y + 10" 40, -100
+# func = "5x^2 + y^2 + 4xy - 14x - 6y + 20" x0 = 0, 10 -    x -3, 3 - y 0, 11   BEST EXAMPLE
 func = "(1-x)^2 + 100*(y-x^2)^2"
 
 parser = FunctionHandler()
@@ -18,7 +23,7 @@ gss.set_parameters(-1, 1, 0.000001)
 
 msd = SteepestDescentMethod(gss, parser)
 msd.set_function_handler(parser)
-msd.set_parameters((-0.5, 0.5), 0.00001)
+msd.set_parameters((-0.5, 0.5), 0.001)
 
 if __name__ == '__main__':
     app = QApplication([])
